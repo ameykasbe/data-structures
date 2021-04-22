dbaf06d3539e7ce3f95ef864f25dbc814cffcc99
Amey Kasbe <amey.kasbe@gmail.com>
2021-04-22 19:58:42 +0530

    Insertion after a node

--- linked_list/23. dll_insertion.py
+++ linked_list/23. dll_insertion.py
@@ -13,9 +13,21 @@ class DoublyLinkedList:
         ref = self.head
         if ref == None:
             print("DLL empty.")
+            return
+        print("Forward traversal:")
         while(ref):
             print(ref.data)
+            if ref.next == None:
+                last = ref
             ref = ref.next
+
+        print("Reverse traversal:")
+
+        while(last.next):
+            last = last.next
+        while(last):
+            print(last.data)
+            last = last.prev
 
     def push(self, new_data):
         nn = Node(new_data)
@@ -37,6 +49,30 @@ class DoublyLinkedList:
         nn.prev = ref
         ref.next = nn
 
+    def insert_after(self, key, data):
+        nn = Node(data)
+        ref = self.head
+        if ref == None:
+            print("LL Empty")
+            return
+
+        while(ref):
+            if ref.data == key:
+                break
+            ref = ref.next
+        if ref.next == None:
+            ref.next = nn
+            nn.prev = ref
+            return
+        if ref:
+            nn.next = ref.next
+            nn.prev = ref
+            ref.next.prev = nn
+            ref.next = nn
+            return
+        print("Key not found!")
+        return
+
 
 dll = DoublyLinkedList()
 dll.append(10)
@@ -44,5 +80,7 @@ dll.append(20)
 dll.append(30)
 dll.append(40)
 dll.append(50)
+
+dll.insert_after(50, 55)
 
 dll.print_list()

26cd7863c284bc5e5d21ffdfb15dcacfa92fd665
Amey Kasbe <amey.kasbe@gmail.com>
2021-05-08 19:45:57 +0530

    Deletion

---
+++ 5. binary_tree/6. deletion.py
@@ -0,0 +1,150 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class Queue:
+    def __init__(self, capacity):
+        self.queue = [None] * capacity
+        self.front = 0
+        self.rear = capacity - 1
+        self.size = 0  # Actual size = Number of elements present
+        self.capacity = capacity
+
+    def is_empty(self):
+        return self.size == 0
+
+    def is_full(self):
+        return self.size == self.capacity
+
+    def enqueue(self, data):
+        if self.is_full():
+            print("Queue full.")
+            exit(1)
+        self.rear = (self.rear + 1) % self.capacity
+        self.queue[self.rear] = data
+        self.size += 1
+
+    def dequeue(self):
+        if self.is_empty():
+            print("Empty queue.")
+            exit(1)
+        temp = self.queue[self.front]
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return temp
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def height(self, root):
+        if not root:
+            return 0
+
+        lheight = self.height(root.left)
+        rheight = self.height(root.right)
+
+        if lheight > rheight:
+            return lheight + 1
+        return rheight + 1
+
+    def level_order_traversal(self):
+        height = self.height(self.root)
+        max_nodes = 2**height - 1
+        queue = Queue(max_nodes)
+
+        if self.root is None:
+            print("Tree empty.")
+            return
+
+        queue.enqueue(self.root)
+
+        while(not queue.is_empty()):
+            node = queue.dequeue()
+            print(node.data)
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+
+    def insertion(self, data):
+        height = self.height(self.root)
+        max_nodes = 2**height - 1
+        queue = Queue(max_nodes)
+
+        nn = Node(data)
+
+        if self.root is None:
+            self.root = nn
+            return
+
+        queue.enqueue(self.root)
+
+        while(not queue.is_empty()):
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            else:
+                node.left = nn
+                break
+            if node.right:
+                queue.enqueue(node.right)
+            else:
+                node.right = nn
+                break
+
+    def deletion(self, key):
+        height = self.height(self.root)
+        max_nodes = 2**height - 1
+        queue = Queue(max_nodes)
+
+        if not self.root:
+            print("Linked list empty.")
+            return
+
+        if self.root == key and self.root.left == None and self.root.right == None:
+            self.root = None
+            return
+
+        queue.enqueue(self.root)
+
+        parent = self.root
+        key_node = None
+        while(not queue.is_empty()):
+            node = queue.dequeue()
+            if node.data == key:
+                key_node = node
+            if node.left:
+                parent = node
+                queue.enqueue(node.left)
+            if node.right:
+                parent = node
+                queue.enqueue(node.right)
+
+        if not key_node:
+            print("Node not found.")
+            return
+
+        key_node.data = node.data
+        if parent.left is node:
+            parent.left = None
+        else:
+            parent.right = None
+
+
+if __name__ == "__main__":
+    tree = Tree()
+    tree.insertion(1)
+    tree.insertion(2)
+    tree.insertion(3)
+    tree.insertion(4)
+    tree.insertion(5)
+    tree.level_order_traversal()
+    tree.deletion(1)
+    tree.level_order_traversal()

3bb35b6ed3f0cc560b2a2a87f470e314cb71883a
Amey Kasbe <amey.kasbe@gmail.com>
2021-06-04 12:26:10 +0530

    git add.

---
+++ 5. binary_tree/25. level_order_spiral.py
@@ -0,0 +1,132 @@
+class Queue:
+    def __init__(self, capacity):
+        self.queue = [None] * capacity
+        self.front = 0
+        self.rear = capacity - 1
+        self.size = 0  # Actual size = Number of elements present
+        self.capacity = capacity
+
+    def is_empty(self):
+        return self.size == 0
+
+    def is_full(self):
+        return self.size == self.capacity
+
+    def enqueue(self, data):
+        if self.is_full():
+            print("Queue full.")
+            exit(1)
+        self.rear = (self.rear + 1) % self.capacity
+        self.queue[self.rear] = data
+        self.size += 1
+
+    def dequeue(self):
+        if self.is_empty():
+            print("Empty queue.")
+            exit(1)
+        temp = self.queue[self.front]
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return temp
+
+    def print_queue(self):
+        temp = self.front
+        for i in range(self.size):
+            print(self.queue[temp + i], end=" ")
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def level_order(self):
+        if self.root is None:
+            return
+        queue = Queue(20)
+        queue.enqueue(self.root)
+
+        while(not queue.is_empty()):
+            node = queue.dequeue()
+            print(node.data, end=" ")
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+
+    def insert(self, data):
+        new_node = Node(data)
+        if self.root is None:
+            self.root = new_node
+            return
+        queue = Queue(20)
+        queue.enqueue(self.root)
+
+        while(not queue.is_empty()):
+            node = queue.dequeue()
+            if node.left:
+                queue.enqueue(node.left)
+            else:
+                node.left = new_node
+                break
+
+            if node.right:
+                queue.enqueue(node.right)
+            else:
+                node.right = new_node
+                break
+
+    def spiral(self):
+        if self.root is None:
+            return
+
+        queue = Queue(20)
+        queue.enqueue(self.root)
+        ltr = False
+
+        while(not queue.is_empty()):
+            if ltr:
+                for i in range(queue.front, queue.rear+1):
+                    print(queue.queue[i].data, end=" ")
+            else:
+                for i in range(queue.rear, queue.front-1, -1):
+                    print(queue.queue[i].data, end=" ")
+            ltr = not ltr
+
+            # If you want to work on the levels one by one.
+            # For loop takes the range elements once at the beginning only. Doesn't matter what the size is.
+            for i in range(queue.size):
+                node = queue.dequeue()
+                if node.left:
+                    queue.enqueue(node.left)
+                if node.right:
+                    queue.enqueue(node.right)
+
+
+if __name__ == "__main__":
+    tree = Tree()
+    tree.insert(1)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(4)
+    tree.insert(5)
+    tree.insert(6)
+    tree.insert(7)
+    tree.root.right.right.left = Node(8)
+    tree.root.right.right.right = Node(9)
+    '''
+          1
+        /    \ 
+       2      3
+      / \   /   \
+     4   5  6   7
+               /  \ 
+              8   9 
+    '''
+    tree.spiral()
